acc644c5ca1681e4c3204619e9be090598fea61b
Zachary White <zacwhite@my365.bellevue.edu>
2025-04-03 19:45:30 -0400

    Create Error_Exercise_M4.cpp
    Updated Pathing as part of the structure issue

---
+++ Module 4/Error_Exercise_M4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Establishing "i" as naming variable depicting number
+    int i; 
+}
+    // Referencing example 5-12 within our powerpoint slide 33 (for) repetition
+    for (i = 1; i <= 12; i+-) {
+     cout << "Number: " << i << endl;
+      
+    return 0;
+}
+
+     // 2  simple syntax errors exist within this code good luck!
