19a6e965017c7e1d90bd3c6df7ead35bf66d5350
Zachary White <zacwhite@my365.bellevue.edu>
2025-04-17 17:55:01 -0400

    Create M6Discussion.cpp
    Simple CPP Enumeration example, for discussion post

---
+++ Module 6/M6Discussion.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+enum Color { RED, GREEN, BLUE };
+
+int main() {
+    Color myColor = YELLOW;
+
+    if (mycolor == RED) {
+        cout << "The color is red." << endl;
+    }
+
+    return 0;
+}
